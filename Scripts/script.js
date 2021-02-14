6295ae26684fa850a73cd74550a979777fddc7ca
Saleh Jomar <sdsalugandang@up.edu.ph>
2021-02-14 14:20:15 +0800

    Create/Clear button.

--- Scripts/script.js
+++ Scripts/script.js
@@ -5,6 +5,7 @@ let pixelSize
 const slider = document.getElementById("slider");
 const output = document.getElementById("slider-value");
 const create = document.getElementById('create');
+const gridContainer = document.querySelector('.grid-container');
 output.innerHTML = slider.value;
 pixelSize = slider.value;
 
@@ -14,7 +15,6 @@ slider.oninput = function() {
 }
 
 function drawGrid(){
-    const gridContainer = document.querySelector('.grid-container');
     gridContainer.setAttribute('style', `grid-template-columns: repeat(${pixelSize}, 1fr); grid-template-rows: repeat(${pixelSize}, 1fr);`);
     for (let i=0; i < (pixelSize*pixelSize); i++){
         const grid = document.createElement('div');
@@ -29,7 +29,6 @@ function drawGrid(){
 
 create.onclick = () => {
     const grids = document.querySelectorAll('.grid');
-    const gridContainer = document.querySelector('.grid-container');
     grids.forEach(div => {
         gridContainer.removeChild(div);
     });
