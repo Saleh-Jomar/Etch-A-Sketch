6bb6581208066a9d93c2ef0d81730ac4554b8446
Saleh Jomar <sdsalugandang@up.edu.ph>
2021-02-14 13:55:52 +0800

    Created the drawing box. Color variability to follow.

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,10 +1,40 @@
+window.addEventListener('load',drawGrid)
+
 let pixelSize
+
 const slider = document.getElementById("slider");
 const output = document.getElementById("slider-value");
+const create = document.getElementById('create');
 output.innerHTML = slider.value;
 pixelSize = slider.value;
 
 slider.oninput = function() {
     output.innerHTML = this.value;
     pixelSize = this.value;
+}
+
+function drawGrid(){
+    const gridContainer = document.querySelector('.grid-container');
+    gridContainer.setAttribute('style', `grid-template-columns: repeat(${pixelSize}, 1fr); grid-template-rows: repeat(${pixelSize}, 1fr);`);
+    for (let i=0; i < (pixelSize*pixelSize); i++){
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        gridContainer.appendChild(grid);
+    }
+    const grids = document.querySelectorAll('.grid');
+    grids.forEach(div => {
+        div.addEventListener('mouseover',color);
+    });
+}
+
+create.onclick = () => {
+    const grids = document.querySelectorAll('.grid');
+    const gridContainer = document.querySelector('.grid-container');
+    grids.forEach(div => {
+        gridContainer.removeChild(div);
+    });
+    drawGrid()
+}
+function color(e){
+    this.setAttribute('style', 'background-color: black;')
 }
