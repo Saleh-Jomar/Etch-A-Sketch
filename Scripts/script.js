eff7164f1ce9bc715ec5b9596056d03dcfc02cb8
Saleh Jomar <sdsalugandang@up.edu.ph>
2021-02-14 18:06:26 +0800

    Added color picker button functionality, but for now only the eraser and black works. Rainbow, grayscale, and color-picker to follow.

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,18 +1,27 @@
 window.addEventListener('load',drawGrid)
 
 let pixelSize
+let color
 
 const slider = document.getElementById("slider");
 const output = document.getElementById("slider-value");
 const create = document.getElementById('create');
+const colorButton = document.querySelectorAll('.color-button')
 const gridContainer = document.querySelector('.grid-container');
 output.innerHTML = slider.value;
 pixelSize = slider.value;
+color = 'black';
 
 slider.oninput = function() {
     output.innerHTML = this.value;
     pixelSize = this.value;
 }
+
+colorButton.forEach(button => {
+    button.onclick = () => {
+        color = button.value;
+    };
+})
 
 function drawGrid(){
     gridContainer.setAttribute('style', `grid-template-columns: repeat(${pixelSize}, 1fr); grid-template-rows: repeat(${pixelSize}, 1fr);`);
@@ -23,7 +32,7 @@ function drawGrid(){
     }
     const grids = document.querySelectorAll('.grid');
     grids.forEach(div => {
-        div.addEventListener('mouseover',color);
+        div.addEventListener('mouseover',colorGrid);
     });
 }
 
@@ -34,6 +43,12 @@ create.onclick = () => {
     });
     drawGrid()
 }
-function color(e){
-    this.setAttribute('style', 'background-color: black;')
+function colorGrid(e){
+    switch(color){
+        case 'black':
+            this.setAttribute('style', 'background-color: black;');
+            break;
+        case 'eraser':
+            this.setAttribute('style', 'background-color: white;');
+    }    
 }
